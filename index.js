40c4ac951747fee680756e4b3bd43048996a805f
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-06 10:33:47 +0530

    Add initial files and configurations for various packages, including Mongoose and Sift

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
+
+// mongoose connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/node-mongo")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+// schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// modal
+const User = mongoose.model("User", userSchema);
 
 // Middlewares
 
@@ -17,12 +53,14 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+
   const html = `
   ${users
     .map(
       (user) =>
-        `<ul> <li>${user.first_name}</li> <li>${user.last_name}</li> </ul>`
+        `<ul> <li>${user.firstName}</li> <li>${user.lastName}</li> </ul>`
     )
     .join("")}
   `;
@@ -30,32 +68,46 @@ app.get("/users", (req, res) => {
 });
 
 // rest api
-app.get("/api/users", (req, res) => {
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
   return res.json(users);
 });
 
 // dynamic routing
 app
-  .route("api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+  .route("/api/users/:id")
+  .get(async (req, res) => {
+    console.log(req.params.id);
+    const user = await User.findById(req.params.id);
     console.log(user);
     return res.json(user);
   })
-  .patch((req, res) => {
-    return res.json({ status: "Pending" });
+  .patch(async (req, res) => {
+    await User.findByIdAndUpdate(req.params.id, { lastName: "nasar" });
+    return res.json({ status: "success" });
   })
-  .delete((req, res) => {
-    return res.json({ status: "Pending" });
+  .delete(async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({ status: "Success" });
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "success", id: users.length });
-  });
+
+  if (
+    !body ||
+    !body.email ||
+    !body.firstName ||
+    !body.lastName ||
+    !body.jobTitle ||
+    !body.gender
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const result = await User.create(body);
+  return res.status(201).json(result);
+  f;
 });
 
 app.listen(PORT, () => {
