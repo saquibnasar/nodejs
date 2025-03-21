2d6ab811ccd64debbc8cdc36b556d99e0108c607
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-03-21 19:52:18 +0530

    Add EJS view engine, home page, and update package.json dependencies

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const { connectMongoDb } = require("./connection");
 
 const { logReqRes } = require("./middlewares");
@@ -8,6 +9,10 @@ const userRouter = require("./routes/user");
 const app = express();
 const PORT = 3000;
 
+// view engine setup
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 // mongoose connection
 connectMongoDb("mongodb://127.0.0.1:27017/node-mongo").then(() =>
   console.log("Connected to MongoDB")
@@ -16,9 +21,13 @@ connectMongoDb("mongodb://127.0.0.1:27017/node-mongo").then(() =>
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"));
+app.use(express.json());
 
 // Routes
 app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
